284869edf66444b537102fa39fd4862cc4bf9992
Edwin de Jong <edwin@ideffector.no>
2023-01-25 20:13:37 +0100

    add dllexport to the functions in the header file to match the source file

--- src/amod/amod.h
+++ src/amod/amod.h
@@ -2,14 +2,14 @@
  * Part of Astonia Client (c) Daniel Brockhaus. Please read license.txt.
  */
 
-void amod_init(void);
-void amod_exit(void);
-void amod_gamestart(void);
-void amod_frame(void);
-void amod_tick(void);
-int amod_display_skill_line(int v,int base,int curr,int cn,char *buf);
-void amod_mouse_move(int x,int y);
-int amod_mouse_click(int x,int y,int what);     // return true if mouse click should NOT be processed by the client
+__declspec(dllexport) void amod_init(void);
+__declspec(dllexport) void amod_exit(void);
+__declspec(dllexport) void amod_gamestart(void);
+__declspec(dllexport) void amod_frame(void);
+__declspec(dllexport) void amod_tick(void);
+__declspec(dllexport) int amod_display_skill_line(int v,int base,int curr,int cn,char *buf);
+__declspec(dllexport) void amod_mouse_move(int x,int y);
+__declspec(dllexport) int amod_mouse_click(int x,int y,int what);     // return true if mouse click should NOT be processed by the client
 
 // --------- Client exported functions -----------
 
